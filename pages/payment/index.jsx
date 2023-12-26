b02fabff805cb9832e3502079bfe809459d8249c
Sami <sboudouk@student.42.fr>
2023-12-26 22:17:34 +0100

    🐛 fix: wrong case

--- pages/payment/index.jsx
+++ pages/payment/index.jsx
@@ -1,24 +1,35 @@
-
-
-import Layout from '../../components/Layout';
-import NavHeader from '../../components/NavHeader';
-import Footer from '../../components/Footer';
-import Component from '../../containers/payment';
-import { handlePageRedirect, isObjectEmpty, parseCookies } from '../../helpers/functions';
-import { NAV_HEADER_FURNITURES_BUY_STEPS } from '../../helpers/constants';
+import Layout from "../../components/Layout";
+import NavHeader from "../../components/NavHeader";
+import Footer from "../../components/Footer";
+import Component from "../../containers/Payment";
+import {
+  handlePageRedirect,
+  isObjectEmpty,
+  parseCookies,
+} from "../../helpers/functions";
+import { NAV_HEADER_FURNITURES_BUY_STEPS } from "../../helpers/constants";
 
 const PaymentPage = ({ cookies }) => (
-  <Layout cookies={cookies} title='Paiement' pageId='payment' withoutHeader>
-    <NavHeader secondary initialStep={2} steps={NAV_HEADER_FURNITURES_BUY_STEPS} />
+  <Layout cookies={cookies} title="Paiement" pageId="payment" withoutHeader>
+    <NavHeader
+      secondary
+      initialStep={2}
+      steps={NAV_HEADER_FURNITURES_BUY_STEPS}
+    />
     <Component />
     <Footer />
   </Layout>
-)
+);
 
-export const getServerSideProps = async context => {
+export const getServerSideProps = async (context) => {
   const cookies = await parseCookies(context?.req);
   const parsedCookies = cookies || {};
-  return handlePageRedirect(parsedCookies, 'private', '', parsedCookies?.order?.items?.length);
-}
+  return handlePageRedirect(
+    parsedCookies,
+    "private",
+    "",
+    parsedCookies?.order?.items?.length
+  );
+};
 
 export default PaymentPage;
