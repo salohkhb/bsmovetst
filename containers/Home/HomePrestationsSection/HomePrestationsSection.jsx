5915b7eb6b5b3ce1b806019d9502fa8c7097f910
Perso <perso@MacBook-Pro-de-Mehdi.local>
2024-02-04 14:47:57 +0100

    🐛 Fix some texts and paddings

--- containers/Home/HomePrestationsSection/HomePrestationsSection.jsx
+++ containers/Home/HomePrestationsSection/HomePrestationsSection.jsx
@@ -1,32 +1,50 @@
-import Routes from '../../../helpers/routes';
-import styles from '../index.module.css';
-import messages from '../messages';
-import PrestationCategoryPreview from '../PrestationCategoryPreview/PrestationCategoryPreview';
+import Routes from "../../../helpers/routes";
+import styles from "../index.module.css";
+import messages from "../messages";
+import PrestationCategoryPreview from "../PrestationCategoryPreview/PrestationCategoryPreview";
 
 const categories = [
-  { href: Routes.ESTIMATE_DETAILS_PAGE,
-    title: messages.prestationsSection.categories.moving, src: '/images/prestation_1.png' },
-  { href: Routes.VEHICLE_RENT_PAGE, title: messages.prestationsSection.categories.vehicleRent, src: '/images/prestation_2.png' },
-  { href: Routes.FURNITURES_BUY_PAGE,
-    title: messages.prestationsSection.categories.furnituresBuy, src: '/images/prestation_3.png' },
-]
+  {
+    href: Routes.ESTIMATE_DETAILS_PAGE,
+    title: messages.prestationsSection.categories.moving,
+    src: "/images/prestation_1.png",
+  },
+  {
+    href: Routes.VEHICLE_RENT_PAGE,
+    title: messages.prestationsSection.categories.vehicleRent,
+    src: "/images/prestation_2.png",
+  },
+  {
+    href: Routes.FURNITURES_BUY_PAGE,
+    title: messages.prestationsSection.categories.furnituresBuy,
+    src: "/images/prestation_3.png",
+  },
+];
 
 const HomePrestationsSection = () => (
-  <div className={styles.prestations_section}>
-    <div className={styles.section_title}>{messages.prestationsSection.title}</div>
-    <div className={styles.prestation_section_subtitle_container}>
-      <div className={styles.section_subtitle}>{messages.prestationsSection.subtitles.leftPart}</div>
-      <div className={styles.bsmove_title}>{messages.prestationsSection.subtitles.title}</div>
+  <section className={styles.prestations_section}>
+    <div className={styles.section_title}>
+      {messages.prestationsSection.title}
     </div>
-    <div className={styles.prestation_section_content}>
-      <div className={styles.section_content}>{messages.prestationsSection.content}</div>
-    </div>
-    <div className={styles.prestation_section_categories_container}>
+    <section className={styles.prestation_section_subtitle_container}>
+      <div className={styles.section_subtitle}>
+        {messages.prestationsSection.subtitles.leftPart}
+      </div>
+      <div className={styles.bsmove_title}>
+        {messages.prestationsSection.subtitles.title}
+      </div>
+    </section>
+    <section className={styles.prestation_section_content}>
+      <div className={styles.section_content}>
+        {messages.prestationsSection.content}
+      </div>
+    </section>
+    <section className={styles.prestation_section_categories_container}>
       {categories.map((category) => (
         <PrestationCategoryPreview key={category.title} category={category} />
       ))}
-    </div>
-  </div>
+    </section>
+  </section>
 );
 
 export default HomePrestationsSection;
--- containers/Home/messages.js
+++ containers/Home/messages.js
@@ -23,7 +23,7 @@ export default {
       title: "BS Move",
     },
     content:
-      "Bsmove vous offre des options flexibles pour votre déménagement. Choisissez parmi le déménagement complet , la location de monte-meuble, de camion avec chauffeur, ou l’achat de matériel. Personnalisez votre prestation selon vos besoins. Avec Bsmove, déménager devient simple et sur mesure.",
+      "Bsmove vous offre des options flexibles pour votre déménagement. Choisissez parmi le déménagement complet , la location de monte-meuble, de camion avec chauffeur, ou l’achat de matériel. Personnalisez votre prestation selon vos besoins. Avec Bsmove, Le déménagement sur mesure devient simple.",
     categories: {
       moving: "Déménagement",
       vehicleRent: "Location véhicules",
