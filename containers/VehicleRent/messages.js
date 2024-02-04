1b5e2d148545694fa3a46f88bf511f16bbe28191
Perso <perso@MacBook-Pro-de-Mehdi.local>
2024-02-04 14:47:12 +0100

    🐛 Add some texts

--- containers/VehicleRent/messages.js
+++ containers/VehicleRent/messages.js
@@ -5,7 +5,7 @@ export default {
       title:
         "Comment obtenir un devis en ligne rapidement pour mon déménagement avec Bsmove ?",
       content:
-        "Pour obtenir rapidement un devis en ligne pour votre déménagement avec Bsmove, il vous suffit de visiter notre site web. Sur la page d’accueil, vous trouverez un formulaire de devis simple et rapide à remplir. Indiquez les détails de votre déménagement, tels que la destination, la date prévue, et le type de services dont vous avez besoin. Une fois le formulaire soumis, notre équipe vous fournira un devis personnalisé dans les plus brefs délais.",
+        "Pour obtenir rapidement un devis en ligne pour votre déménagement avec Bsmove, il vous suffit de visiter notre site web. Sur la page d’accueil, vous trouverez un formulaire de devis simple et rapide à remplir. Indiquez les détails de votre déménagement, tels que la destination, la date prévue et le type de services dont vous avez besoin. Une fois le formulaire soumis, notre équipe vous fournira un devis personnalisé dans les plus brefs délais.",
     },
     secondSection: {
       title: "Quels services sont inclus dans un déménagement standard ?",
@@ -21,7 +21,7 @@ export default {
     fourthSection: {
       title: "Pourquoi choisir Bsmove déménagement ?",
       content:
-        "Choisir Bsmove déménagement présente de nombreux avantages. Notre équipe professionnelle offre une expertise en emballage sécurisé, assure une manipulation professionnelle de vos biens, assure un transport efficace, et fournit une assurance pour garantir la sécurité de vos biens pendant tout le processus de déménagement. Optez pour Bsmove pour une expérience sans souci et fiable.",
+        "Choisir Bsmove déménagement présente de nombreux avantages. Notre équipe professionnelle offre une expertise en emballage sécurisé, assure une manipulation professionnelle de vos biens, assure un transport efficace et fournit une assurance pour garantir la sécurité de vos biens pendant tout le processus de déménagement. Optez pour Bsmove pour une expérience sans souci et fiable.",
     },
   },
 };
