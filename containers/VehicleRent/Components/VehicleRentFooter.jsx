b4d3e7cf8181496f6a0994df930ad320684244e0
Perso <perso@MacBook-Pro-de-Mehdi.local>
2024-02-04 14:47:34 +0100

    🐛 Fix button redirect and correct wrong text

--- containers/VehicleRent/Components/VehicleRentFooter.jsx
+++ containers/VehicleRent/Components/VehicleRentFooter.jsx
@@ -1,14 +1,31 @@
-import styles from '../index.module.css';
-import {Button} from '@mui/material';
+import styles from "../index.module.css";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import Routes from "../../../helpers/routes";
 
 const VehicleRentFooter = () => {
   return (
     <footer className={styles.vehicle_rent_footer__main}>
-      <article style={{ color: "rgb(56, 199, 152)" }}>{"Vous n'êtes pas sur ?"}</article>
-      <h1 className={styles.vehicle_rent_footer__content} style={{ color: "white" }}>Calculer votre volume gratuitement grâce à notre outil calculateur de volume</h1>
-      <Button variant="outlined" sx={{ color: 'white', border: '1px solid white' }}>Calculer mon vulume</Button>
+      <article style={{ color: "rgb(56, 199, 152)" }}>
+        {"Vous n'êtes pas sur ?"}
+      </article>
+      <h1
+        className={styles.vehicle_rent_footer__content}
+        style={{ color: "white" }}
+      >
+        Calculer votre volume gratuitement grâce à notre outil calculateur de
+        volume
+      </h1>
+      <Link href={Routes.VOLUME_CALCULATOR_PAGE}>
+        <Button
+          variant="outlined"
+          sx={{ color: "white", border: "1px solid white" }}
+        >
+          Calculer mon volume
+        </Button>
+      </Link>
     </footer>
-  )
-}
+  );
+};
 
-export default VehicleRentFooter
+export default VehicleRentFooter;
