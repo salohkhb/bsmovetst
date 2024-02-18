3eea0f6a6ba8297835953bff1927283e6c2d6a6c
Perso <perso@MacBook-Pro-de-Mehdi.local>
2024-02-18 22:35:14 +0100

    🐛 Test to see if it resolves the loading bug

--- components/RouterTransitions/index.jsx
+++ components/RouterTransitions/index.jsx
@@ -1,30 +1,32 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { isObjectEmpty } from '../../helpers/functions';
-import { useLoading } from '../../hooks/loading';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { isObjectEmpty } from "../../helpers/functions";
+import { useLoading } from "../../hooks/loading";
 
 const RouterTransitions = () => {
-  const { setGlobalLoading } = useLoading();
+  const { setGlobalLoading, globalLoading } = useLoading();
   const router = useRouter();
 
   function handleRouterChangeStart() {
+    if (globalLoading) return;
     setGlobalLoading(true);
   }
 
   function handleRouterChangeEnd() {
+    if (!globalLoading) return;
     setGlobalLoading(false);
   }
 
   useEffect(() => {
-    if (isObjectEmpty(router)) return ;
-    router.events.on('routeChangeStart', handleRouterChangeStart);
-    router.events.on('routeChangeComplete', handleRouterChangeEnd)
+    if (isObjectEmpty(router)) return;
+    router.events.on("routeChangeStart", handleRouterChangeStart);
+    router.events.on("routeChangeComplete", handleRouterChangeEnd);
     return () => {
-      router.events.off('routeChangeStart', handleRouterChangeStart);
-      router.events.off('routerChangeComplete', handleRouterChangeEnd)
-    }
-  }, [router])
+      router.events.off("routeChangeStart", handleRouterChangeStart);
+      router.events.off("routerChangeComplete", handleRouterChangeEnd);
+    };
+  }, [router]);
   return null;
-}
+};
 
 export default RouterTransitions;
