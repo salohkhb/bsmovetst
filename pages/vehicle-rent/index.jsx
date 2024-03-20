8203f4d028dec5fe17451e5ce9e6ebebbf95c2f0
hafed <hafed.zouyed@univ-constantine2.dz>
2024-03-20 16:55:17 +0100

    I fixed vehicule-rent url

--- pages/vehicle-rent/index.jsx
+++ pages/vehicle-rent/index.jsx
@@ -22,11 +22,11 @@ const VehicleRentPage = ({ cookies }) => {
       cookies={cookies}
       title={`BS Move - Choix de la location${rentType ? ` de ${rentType}` : ""}`}
       description="Louez facilement des véhicules pour votre déménagement ou vos déplacements à des tarifs compétitifs."
-      keywords="vehicle, rent, vehicle rent,location, véhicule, camion, déménagement, services, transports, monte-meubles, déménagement, déplacement, tarifs compétitifs, location de camion, location de monte-meubles, location de véhicule"
+      keywords="location, rent, véhicule, camion, déménagement, services, transports, monte-meubles, déménagement, déplacement, tarifs compétitifs, location de camion, location de monte-meubles, location de véhicule"
       pageId="vehicle-and-lift-rent"
     >
       <Head>
-        <link rel="canonical" href="https://www.bsmove.com/BS Move Déménagement - Location de véhicules pour votre déménagement" />
+        <link rel="canonical" href="https://www.bsmove.com/vehicle-rent" />
       </Head>
       <NavHeader />
       <Component />
