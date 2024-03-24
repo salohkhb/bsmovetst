f516c580ca1a851f02bea19b187d37ccd112d849
hafed <hafed.zouyed@univ-constantine2.dz>
2024-03-24 01:23:09 +0100

    changing the contact route name to test the seo

--- pages/Contactez-nous/index.jsx
+++ pages/Contactez-nous/index.jsx
@@ -9,7 +9,7 @@ import { handlePageRedirect, parseCookies } from '../../helpers/functions';
 const ContactPage = ({ cookies }) => (
   <Layout cookies={cookies} title='BS Move Déménagement - Contactez-nous'
     description="Besoin d'aide pour votre déménagement ? Contactez BS Move dès aujourd'hui pour obtenir une estimation, poser vos questions, ou demander des renseignements. Notre équipe d'assistance est là pour vous offrir tous les services et informations nécessaires. Que ce soit pour un devis, une assistance, ou toute autre demande, nous sommes prêts à répondre à vos besoins."
-    keywords="contacter, contact, contactez-nous, Nous contacter, déménagement, services, informations, Besoin d'aide, Demande de renseignements, assistance, devis, estimation, Renseignements, Questions, Assistance"
+    keywords="contactez-nous, contacter, contact, déménagement, services, informations, Besoin d'aide, Demande de renseignements, assistance, devis, estimation, Renseignements, Questions, Assistance"
     pageId='contact'>
     <NavHeader />
     <Component />
