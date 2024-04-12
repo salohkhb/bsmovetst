9da308a7490986b7071dd8414028562f48ba6e6a
hafed <hafed.zouyed@univ-constantine2.dz>
2024-04-12 21:16:01 +0100

    I added meaux page

---
+++ containers/Meaux/messages.js
@@ -0,0 +1,63 @@
+export default {
+  estimateSection: {
+    title: "BS MOVE VOTRE Déménageur à Meaux 77100",
+    content: {
+      main: "Profitez d'un déménagement à Meaux qui répond parfaitement à vos attentes.",
+      secondary:
+        "Pour déménager à Meaux, il est probable que vous deviez faire appel la société de déménageurs BS Move demenagement pour bénéficier d'une prestation fiable et de qualité. Déménager à Meaux avec le groupe BS Move vous garantit l'expertise d'une équipe professionnelle à un prix abordable. Un conseiller se rendra chez vous pour étudier votre projet de déménagement. Cette analyse permettra au groupe BS Move de mettre en œuvre toutes les mesures nécessaires et de vous fournir un devis précis.",
+    },
+    actions: {
+      leftButton: "Obtenir un devis",
+      rightButton: "En savoir plus",
+    },
+  },
+  advantagesSection: {
+    title: "avantages",
+    subtitle: "Pourquoi choisir BS Move",
+    advantages: {
+      first: {
+        title: "Gain de temps",
+        content:
+          "Notre équipe expérimentée prend en charge l'emballage efficace de vos biens, la coordination logistique complète, et les formalités administratives. Vous économisez ainsi un temps précieux, pouvant vous concentrer sur d'autres aspects importants de votre transition.",
+      },
+      second: {
+        title: "Accompagnement expert",
+        content:
+          "Nous vous accompagnons à chaque étape de votre déménagement. Du premier contact à l'installation dans votre nouveau lieu de vie, nos experts sont là pour répondre à vos questions, résoudre les problèmes potentiels et assurer une expérience sans stress.",
+      },
+      third: {
+        title: "Service professionnel",
+        content:
+          "En choisissant Bs Move Déménagement , vous optez pour un service professionnel de qualité. Notre équipe dévouée garantit un emballage soigné, un transport sécurisé et une attention méticuleuse aux détails. Vous pouvez avoir confiance en notre engagement envers la satisfaction totale de nos clients.",
+      },
+    },
+  },
+  competencesSection: {
+    title: "Location de monte meuble et de camion avec chauffeur meaux",
+    subtitle: "BS Move Location a Meaux, une équipe professionnelle.",
+    contents: {
+      firstPart:
+        "Découvrez notre gamme de services de location à Meaux, offrant une flexibilité totale. Louez un camion avec chauffeur selon vos besoins à Meaux. Chez Bsmove, nous vous garantissons une expérience de déménagement personnalisée à Meaux, avec des solutions de qualité pour chaque étape de votre transition.",
+      secondPart: "Besoin d'un monte-meuble avec opérateur à Meaux ? Nous avons ce qu'il vous faut. Chez Bsmove, nous vous offrons une expérience de location de monte-meuble sur mesure à Meaux, adaptée à vos exigences spécifiques.",
+    },
+    action: "demende la location en ligne",
+  },
+  meubleSection: {
+    title: "Achats de carton de déménagement meaux",
+    content: {
+      main: "Achetez vos cartons de déménagement à Meaux: pour un emballage sûr et efficace",
+      secondary:
+        "Pour vos achats de cartons de déménagement à Meaux, la société BS Move déménagement est votre partenaire idéal pour des fournitures fiables et de qualité. Acheter vos cartons à Meaux avec BS Move vous assure l'accès à des produits adaptés à vos besoins, à un prix compétitif. Un conseiller peut même vous guider dans le choix des matériaux adaptés à la nature de vos biens. Cette attention aux détails garantit que vous disposerez des ressources nécessaires pour un déménagement sécurisé et organisé, avec un devis ajusté à vos exigences.",
+    },
+    actions: {
+      leftButton: "Achats de cartons",
+      rightButton: "En savoir plus",
+    },
+  },
+  gettingStartedSection: {
+    subtitle: "Prêt à déménager ?",
+    content:
+      "Le jour J approche, et nous sommes là pour vous simplifier la vie. De l’emballage méticuleux à une logistique bien huilée, notre équipe expérimentée prend en charge chaque étape. Que vous changiez de quartier ou franchissiez les frontières européennes, nous sommes prêts à rendre votre déménagement sans stress. Donnez le coup d’envoi à cette nouvelle aventure avec la confiance que seul Bsmove peut offrir. Prêt à déménager ? Nous sommes à vos côtés.",
+    action: "Déménageons ensemble",
+  },
+}
