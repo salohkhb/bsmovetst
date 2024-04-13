5a6cc54478c032bbc8c0ef446143c80abf748c34
hafed <hafed.zouyed@univ-constantine2.dz>
2024-04-13 21:04:46 +0100

    adding Chelles page

---
+++ containers/Chelles/index.jsx
@@ -0,0 +1,237 @@
+import HomePrestationsSection from "../Home/HomePrestationsSection/HomePrestationsSection";
+import styles from "./index.module.css"
+import Image from "next/legacy/image";
+import Button from "../../components/Button";
+import { useGlobal } from "../../hooks/global";
+import { useRouter } from "next/router";
+import messages from "./messages";
+import Routes from "../../helpers/routes";
+import styled from "styled-components";
+import Card from "@mui/material/Card";
+import { useState } from "react";
+  
+const ChellesEstimateSection = () => {
+  const {
+    global: { screenWidth },
+  } = useGlobal();
+  let router = useRouter();
+  return (
+    <div className={styles.estimate_section}>
+      <div className={styles.home_estimate_section_left}>
+        <h1 className={styles.section_title}>
+          {messages.estimateSection.title}
+        </h1>
+        <h2 className={styles.estimate_section_main_content}>
+          {messages.estimateSection.content.main}
+        </h2>
+        <div className={styles.estimate_section_secondary_content}>
+          {messages.estimateSection.content.secondary}
+        </div>
+        <div className={styles.estimate_section_actions_container}>
+          <Button
+            onClick={() => router.push(Routes.ESTIMATE_DETAILS_PAGE)}
+            margin="0 4px 0 0"
+          >
+            {messages.estimateSection.actions.leftButton}
+          </Button>
+        </div>
+      </div>
+      <div className={styles.home_estimate_section_right}>
+        <div className={styles.home_estimate_section_right_img_container}>
+          <Image
+            className={styles.home_estimage_section_right_img_illustration}
+            layout="fill"
+            src="/images/meaux.jpg"
+            alt="homepage_section_estimate"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ChellesCompetencesSection = () => {
+  const router = useRouter();
+  return (
+  <div className={styles.competences_section}>
+    <div className={styles.competences_section_illustration_container}>
+      <div className={styles.competences_section_illustration}>
+        <Image
+          className={styles.prestation_section_category_thumbnail}
+          layout="fill"
+          src="/images/moving_together_illu.png"
+          alt="competences"
+        />
+      </div>
+    </div>
+    <div className={styles.competences_section_infos}>
+      <div className={styles.section_title}>
+        {messages.competencesSection.title}
+      </div>
+      <div className={styles.section_subtitle}>
+        {messages.competencesSection.subtitle}
+      </div>
+      <div className={styles.competences_section_infos_content}>
+        <div>{messages.competencesSection.contents.firstPart}</div>
+        <div>{messages.competencesSection.contents.secondPart}</div>
+      </div>
+      <div className={styles.estimate_section_actions_container}>
+          <Button
+            onClick={() => router.push(Routes.VEHICLE_RENT_PAGE)}
+            margin="0 0 0 4px"
+          >
+            {messages.competencesSection.action}
+          </Button>
+        </div>
+    </div>
+  </div>
+  );
+};
+const S = {};
+
+S.AdvantageCard = styled(Card)`
+  max-width: 376px;
+  min-width: 320px;
+`;
+
+const AdvantageCard = ({ title, content }) => {
+  const [raised, setRaised] = useState(false);
+
+  function toggleRaised() {
+    return setRaised((prevRaised) => !prevRaised);
+  }
+  return (
+    <Card
+      onMouseOver={toggleRaised}
+      onMouseOut={toggleRaised}
+      raised={raised}
+      sx={{ backgroundColor: "transparent !important" }}
+    >
+      <S.AdvantageCard elevation={1}>
+        <div className={styles.advantages_card_container}>
+          <div className={styles.advantages_card_title}>{title}</div>
+          <div className={styles.section_content}>{content}</div>
+        </div>
+      </S.AdvantageCard>
+    </Card>
+  );
+};
+const ChellesAdvantagesSection = () => (
+  <div className={styles.advantages_section}>
+    <div className={styles.section_title}>
+      {messages.advantagesSection.title}
+    </div>
+    <div className={styles.section_subtitle}>
+      {messages.advantagesSection.subtitle}
+    </div>
+    <div className={styles.advantages_section_cards}>
+      <AdvantageCard
+        title={messages.advantagesSection.advantages.first.title}
+        content={messages.advantagesSection.advantages.first.content}
+      />
+      <AdvantageCard
+        title={messages.advantagesSection.advantages.second.title}
+        content={messages.advantagesSection.advantages.second.content}
+      />
+      <AdvantageCard
+        title={messages.advantagesSection.advantages.third.title}
+        content={messages.advantagesSection.advantages.third.content}
+      />
+    </div>
+  </div>
+);
+
+const ChellesMeubleSection = () => {
+  const {
+    global: { screenWidth },
+  } = useGlobal();
+  let router = useRouter();
+  return (
+    <div className={styles.estimate_section}>
+      <div className={styles.home_estimate_section_left}>
+        <h2 className={styles.section_title}>
+          {messages.meubleSection.title}
+        </h2>
+        <h3 className={styles.estimate_section_main_content}>
+          {messages.meubleSection.content.main}
+        </h3>
+        <div className={styles.estimate_section_secondary_content}>
+          {messages.meubleSection.content.secondary}
+        </div>
+        <div className={styles.estimate_section_actions_container}>
+          <Button
+            onClick={() => router.push(Routes.FURNITURES_BUY_PAGE)}
+            margin="0 4px 0 0"
+          >
+            {messages.meubleSection.actions.leftButton}
+          </Button>
+        </div>
+      </div>
+      <div className={styles.home_estimate_section_right}>
+        <div className={styles.home_estimate_section_right_img_container}>
+          <Image
+            className={styles.home_estimage_section_right_img_illustration}
+            layout="fill"
+            src="/images/prestation_3.png"
+            alt="homepage_section_estimate"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+const ChellesGettingStartedSection = () => {
+  const router = useRouter();
+  return (
+    <div className={styles.getting_started_section}>
+      <div className={styles.getting_started_container}>
+        <div className={styles.getting_started_left_container}>
+          <div className={styles.getting_started_infos}>
+            <div className={styles.section_subtitle}>
+              {messages.gettingStartedSection.subtitle}
+            </div>
+            <div className={styles.section_content}>
+              {messages.gettingStartedSection.content}
+            </div>
+          </div>
+          <div className={styles.getting_started_action_container}>
+            <Button
+              onClick={() => router.push(Routes.ESTIMATE_DETAILS_PAGE)}
+              outlined
+            >
+              {messages.gettingStartedSection.action}
+            </Button>
+          </div>
+        </div>
+        <div className={styles.getting_started_section_right_container}>
+          <div
+            className={styles.getting_started_section_illustration_container}
+          >
+            <Image
+              className={styles.getting_started_illustration}
+              layout="fill"
+              src="/images/ready_to_move_truck_illustration.png"
+              alt="getting_started_truck"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+const Meaux = () => {
+  return (
+    <div className={styles.container}>
+      <ChellesEstimateSection />
+      <HomePrestationsSection />
+      <ChellesCompetencesSection />
+      <ChellesAdvantagesSection />
+      <ChellesMeubleSection />
+      <ChellesGettingStartedSection />
+    </div>
+  )
+}
+
+export default Meaux
---
+++ containers/Chelles/messages.js
@@ -0,0 +1,63 @@
+export default {
+  estimateSection: {
+    title: "BS MOVE VOTRE Déménageur à Chelles 77500",
+    content: {
+      main: "Optez pour un déménagement personnalisé à Chelles, parfaitement adapté à vos besoins.",
+      secondary:
+        "Si vous envisagez un déménagement à Chelles, envisagez les services de BS Move Déménagement pour une expérience fiable et de qualité.Déménager à Chelles avec une équipe professionnelle et des tarifs abordables, déménager avec nous garantit une expertise inégalée. Un conseiller évaluera vos besoins et fournira un devis détaillé pour votre déménagement en toute tranquillité.",
+    },
+    actions: {
+      leftButton: "Obtenir un devis",
+      rightButton: "En savoir plus",
+    },
+  },
+  advantagesSection: {
+    title: "avantages",
+    subtitle: "Pourquoi choisir BS Move",
+    advantages: {
+      first: {
+        title: "Gain de temps",
+        content:
+          "Notre équipe expérimentée prend en charge l'emballage efficace de vos biens, la coordination logistique complète, et les formalités administratives. Vous économisez ainsi un temps précieux, pouvant vous concentrer sur d'autres aspects importants de votre transition.",
+      },
+      second: {
+        title: "Accompagnement expert",
+        content:
+          "Nous vous accompagnons à chaque étape de votre déménagement. Du premier contact à l'installation dans votre nouveau lieu de vie, nos experts sont là pour répondre à vos questions, résoudre les problèmes potentiels et assurer une expérience sans stress.",
+      },
+      third: {
+        title: "Service professionnel",
+        content:
+          "En choisissant Bs Move Déménagement , vous optez pour un service professionnel de qualité. Notre équipe dévouée garantit un emballage soigné, un transport sécurisé et une attention méticuleuse aux détails. Vous pouvez avoir confiance en notre engagement envers la satisfaction totale de nos clients.",
+      },
+    },
+  },
+  competencesSection: {
+    title: "Location de monte meuble et de camion avec chauffeur Chelles",
+    subtitle: "BS Move Location a Chelles, une équipe professionnelle.",
+    contents: {
+      firstPart:
+        "Explorez notre sélection de services de location à Chelles, offrant une flexibilité totale. Louez un camion avec chauffeur adapté à vos besoins chez BS Move à Chelles. Nous vous assurons une expérience de déménagement personnalisée, avec des solutions de qualité à chaque étape de votre transition à Chelles.",
+      secondPart: "Besoin d'un monte-meuble avec opérateur à Chelles? Chez BS Move, nous proposons une location sur mesure de monte-meuble à Chelles, parfaitement adaptée à vos exigences spécifiques.",
+    },
+    action: "demende la location en ligne",
+  },
+  meubleSection: {
+    title: "Achats de carton de déménagement Chelles",
+    content: {
+      main: "Procurez-vous vos cartons de déménagement à Chelles pour un emballage sécurisé et efficace.",
+      secondary:
+        "Lors de vos achats de cartons de déménagement à Chelles, BS Move déménagement est le partenaire idéal pour des fournitures fiables et de qualité. Achetez vos cartons à Chelles avec BS Move pour accéder à des produits adaptés à vos besoins, à un prix compétitif. Un conseiller peut vous guider dans le choix des matériaux adaptés à vos biens. Cette attention aux détails garantit un déménagement sécurisé et organisé, avec un devis ajusté à vos exigences.",
+    },
+    actions: {
+      leftButton: "Achats de cartons",
+      rightButton: "En savoir plus",
+    },
+  },
+  gettingStartedSection: {
+    subtitle: "Prêt à déménager ?",
+    content:
+      "Le jour J approche, et nous sommes là pour vous simplifier la vie. De l’emballage méticuleux à une logistique bien huilée, notre équipe expérimentée prend en charge chaque étape. Que vous changiez de quartier ou franchissiez les frontières européennes, nous sommes prêts à rendre votre déménagement sans stress. Donnez le coup d’envoi à cette nouvelle aventure avec la confiance que seul Bsmove peut offrir. Prêt à déménager ? Nous sommes à vos côtés.",
+    action: "Déménageons ensemble",
+  },
+}
