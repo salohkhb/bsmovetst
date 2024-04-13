5bd3497969db3c3f274f5c8d6a0a7f07027d40bf
hafed <hafed.zouyed@univ-constantine2.dz>
2024-04-13 17:54:25 +0100

    adding mofre metadata and making the footer dynamic for testing

--- pages/meaux/index.jsx
+++ pages/meaux/index.jsx
@@ -1,9 +1,13 @@
 import Layout from "../../components/Layout";
 import NavHeader from '../../components/NavHeader';
-import Footer from '../../components/Footer';
+// import Footer from '../../components/Footer';
 import Meaux from "../../containers/Meaux";
 import Head from 'next/head';
+import dynamic from 'next/dynamic'
 
+const DynamicFooter = dynamic(() => import('./../../components/Footer'), {
+  loading: () => <p>Loading...</p>,
+})
 const MeauxPage = ({ cookies }) => {
   return (
     <Layout cookies={cookies}
@@ -14,10 +18,29 @@ const MeauxPage = ({ cookies }) => {
     >
       <Head>
         <link rel="canonical" href="https://www.bsmove.com/meaux" />
+        <meta property="og:url" content="https://www.bsmove.com/meaux"/>
+        <meta property="og:image" content="https://www.bsmove.com/images/logo.png"/>
+        <meta property="og:image:width" content="1200"/>
+        <meta property="og:image:height" content="630"/>
+        <meta property="og:image:alt" content="BS Move Déménagement"/>
+        <meta property="og:site_name" content="BS Move Déménagement"/>
+        <meta property="og:locale" content="fr_FR"/>
+        <meta property="og:locale:alternate" content="en_US"/>
+        {/* Schema.org structured data */}
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: `
+          {
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            "name": "Déménagement Meaux - BS Move Déménagement",
+            "url": "${process.env.NEXT_PUBLIC_WEBSITE_URL}",
+            "logo": "${process.env.NEXT_PUBLIC_WEBSITE_URL}/images/logo.png",
+          }
+        ` }} />
       </Head>
       <NavHeader />
       <Meaux />
-      <Footer />
+      <DynamicFooter />
+
     </Layout>
   )
 }
