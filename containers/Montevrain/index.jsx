3560fdb02b19165fe5fcd72e7abac62d8ecfc01e
hafed <hafed.zouyed@univ-constantine2.dz>
2024-06-22 10:42:19 +0100

    adding saint-fargau-ponthierry and montevrain pages,fixing responsive

---
+++ containers/Montevrain/index.jsx
@@ -0,0 +1,20 @@
+import HomePrestationsSection from "../Home/HomePrestationsSection/HomePrestationsSection";
+import messages from "./messages";
+import EstimateSection from '../../components/Home/EstimateSection/index'
+import CompetencesSection from '../../components/Home/CompetencesSection/index'
+import MeubleSection from '../../components/Home/MeubleSection/index'
+import styles from '../../components/Home/index.module.css'
+
+
+const Montevrain = () => {
+  return (
+    <div className={styles.container}>
+      <EstimateSection messages={messages} />
+      <HomePrestationsSection />
+      <CompetencesSection messages={messages}/>
+      <MeubleSection messages={messages}/>
+    </div>
+  )
+}
+
+export default Montevrain
---
+++ containers/Montevrain/messages.js
@@ -0,0 +1,36 @@
+export default {
+  estimateSection: {
+    title: "BS MOVE VOTRE Déménageur à Montévrain 77144",
+    content: {
+      main: "Découvrez nos services de déménagement à Montévrain, sur mesure pour répondre à vos besoins spécifiques",
+      secondary:
+        "Pour un déménagement sans tracas à Montévrain, optez pour BS Move. Bénéficiez de nos prix compétitifs et de l'expertise de notre équipe pour un service rapide et efficace. Un de nos experts se déplacera chez vous pour évaluer vos besoins, vous conseiller sur les meilleures options, et vous fournir un devis personnalisé. Avec BS Move, profitez d'une prise en charge complète et déménagez en toute sérénité à Montévrain.",
+    },
+    actions: {
+      leftButton: "Obtenir un devis",
+      rightButton: "En savoir plus",
+    },
+  },
+  competencesSection: {
+    title: "Location de monte meuble et de camion avec chauffeur Montévrain",
+    subtitle: "BS Move Location a Montévrain, une équipe professionnelle.",
+    contents: {
+      firstPart:
+        "Explorez notre large éventail de services de location à Montévrain, tous soigneusement élaborés pour satisfaire vos besoins spécifiques. Optez pour la location d'un camion avec chauffeur chez Bsmove, parfaitement adapté à vos exigences. Nous mettons un point d'honneur à vous fournir une expérience de déménagement personnalisée, avec des solutions de haute qualité pour chaque étape de votre projet. Notre engagement est de vous offrir un déménagement sans stress et parfaitement organisé, grâce à notre expertise et notre dévouement à répondre à toutes vos attentes.",
+      secondPart: "Si vous avez besoin de louer un monte-meuble avec opérateur à Montévrain, faites appel à Bsmove. Nous proposons des services personnalisés, spécialement conçus pour répondre précisément à vos besoins spécifiques dans la région.",
+    },
+    action: "demende la location en ligne",
+  },
+  meubleSection: {
+    title: "Achats de carton de déménagement Montévrain",
+    content: {
+      main: "Trouvez vos matériaux d'emballage à Montévrain chez BS Move, votre référence pour des fournitures fiables et de haute qualité.",
+      secondary:
+        "Optez pour BS Move pour obtenir des fournitures de déménagement de premier choix. Notre large gamme de produits garantit un déménagement en toute sécurité et une organisation optimale. Nos experts sont disponibles pour vous conseiller sur les matériaux les plus appropriés à vos biens. Faites dès à présent une demande de devis personnalisé pour votre déménagement à Montévrain, et bénéficiez de notre expertise pour une expérience sans tracas.",
+    },
+    actions: {
+      leftButton: "Achats de cartons",
+      rightButton: "En savoir plus",
+    },
+  },
+}
