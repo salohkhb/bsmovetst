c4e921a843079ba9fb2c1e1612ec7c9042441081
hafed <hafed.zouyed@univ-constantine2.dz>
2024-06-26 11:41:40 +0100

    adding vaires-sur-marne and nemours pages

---
+++ containers/Nemours/messages.js
@@ -0,0 +1,36 @@
+export default {
+  estimateSection: {
+    title: "BS MOVE VOTRE Déménageur à Nemours 77140",
+    content: {
+      main: "Déménagement à Nemours ? Choisissez BS Move ! Profitez de services personnalisés, de la fourniture des cartons au transport.",
+      secondary:
+        "Éliminez le stress du déménagement à Nemours! BS Move s'occupe de tout, de l'évaluation sur place par un expert à la fourniture d'un devis personnalisé, le tout à des tarifs compétitifs. Profitez de notre savoir-faire pour un déménagement clé en main et sans tracas à Nemours. Contactez-nous dès maintenant pour obtenir votre devis gratuit!",
+    },
+    actions: {
+      leftButton: "Obtenir un devis",
+      rightButton: "En savoir plus",
+    },
+  },
+  competencesSection: {
+    title: "Location de monte meuble et de camion avec chauffeur Nemours",
+    subtitle: "BS Move Location a Nemours, une équipe professionnelle.",
+    contents: {
+      firstPart:
+        "Louez un camion avec chauffeur chez BS Move à Nemours, parfaitement adapté à vos besoins. Nos experts offrent des solutions sur mesure et un service attentif, prenant en charge le chargement et le transport. Profitez de votre nouvelle vie à Nemours en toute tranquillité. Contactez-nous dès maintenant pour obtenir un devis gratuit et déménagez sereinement!",
+      secondPart: "Vous avez des objets volumineux ou un accès difficile à Nemours? Louez un monte-meuble avec opérateur chez BS Move! Profitez d'un service sur mesure pour assurer un déménagement sans tracas.",
+    },
+    action: "demende la location en ligne",
+  },
+  meubleSection: {
+    title: "Achats de carton de déménagement Nemours",
+    content: {
+      main: "Besoin de matériel d'emballage pour votre déménagement à Nemours ? Faites appel à BS Move ! Cartons solides.",
+      secondary:
+        "Déménagez sereinement à Nemours avec BS Move ! Accédez à des fournitures de qualité, une large gamme de choix, et les conseils avisés de nos experts pour un déménagement organisé et sans casse. Demandez dès maintenant votre devis gratuit et profitez de notre expertise.",
+    },
+    actions: {
+      leftButton: "Achats de cartons",
+      rightButton: "En savoir plus",
+    },
+  },
+}
