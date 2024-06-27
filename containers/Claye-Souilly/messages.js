cbeaf95b52fdc14700216351840e0f3134d26cd7
hafed <hafed.zouyed@univ-constantine2.dz>
2024-06-27 11:35:46 +0100

    adding claye-souily and moret-loing-et-orvanne

---
+++ containers/Claye-Souilly/messages.js
@@ -0,0 +1,36 @@
+export default {
+  estimateSection: {
+    title: "BS MOVE VOTRE Déménageur à Claye-Souilly 77410",
+    content: {
+      main: "Déménagement à Claye-Souilly ? Comptez sur BS Move ! Nous proposons des services sur mesure, de la fourniture de cartons au transport de vos affaires.",
+      secondary:
+        "Évitez le stress du déménagement à Claye-Souilly grâce à BS Move ! Nous nous occupons de tout, de l'évaluation sur place par un expert à la fourniture d'un devis personnalisé, avec des tarifs compétitifs. Profitez de notre savoir-faire pour un déménagement clé en main et sans tracas à Claye-Souilly. Contactez-nous dès maintenant pour obtenir votre devis gratuit!",
+    },
+    actions: {
+      leftButton: "Obtenir un devis",
+      rightButton: "En savoir plus",
+    },
+  },
+  competencesSection: {
+    title: "Location de monte meuble et de camion avec chauffeur Claye-Souilly",
+    subtitle: "BS Move Location a Claye-Souilly, une équipe professionnelle.",
+    contents: {
+      firstPart:
+        "Choisissez la location d'un camion avec chauffeur chez BS Move à Claye-Souilly, parfaitement adapté à vos besoins particuliers. Nos experts vous offrent des solutions sur mesure et un service attentif, prenant en charge le chargement et le transport de vos biens. Profitez de votre nouvelle vie à Claye-Souilly en toute sérénité. Contactez-nous dès maintenant pour obtenir un devis gratuit et déménagez en toute tranquillité!",
+      secondPart: "Vous avez des objets encombrants ou des accès difficiles à Claye-Souilly ? Optez pour la location d'un monte-meuble avec opérateur chez BS Move ! Profitez d'un service personnalisé pour assurer un déménagement sans tracas.",
+    },
+    action: "demende la location en ligne",
+  },
+  meubleSection: {
+    title: "Achats de carton de déménagement Claye-Souilly",
+    content: {
+      main: "Vous recherchez du matériel d'emballage pour votre déménagement à Claye-Souilly ? BS Move propose des cartons solides et une gamme complète de fournitures adaptées à vos besoins.",
+      secondary:
+        "Déménagez en toute tranquillité à Claye-Souilly avec BS Move ! Profitez de fournitures de qualité, d'un large choix de produits, et des conseils avisés de nos experts pour un déménagement organisé et sécurisé. Obtenez dès maintenant votre devis gratuit et bénéficiez de notre savoir-faire.",
+    },
+    actions: {
+      leftButton: "Achats de cartons",
+      rightButton: "En savoir plus",
+    },
+  },
+}
