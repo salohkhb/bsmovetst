ca789c4aceb66c454daa2ab1c2833fcd0d88fbee
hafed <hafed.zouyed@univ-constantine2.dz>
2024-06-28 15:56:41 +0100

    adding provins and vaux-le-penil pages

---
+++ containers/Vaux-le-Penil/messages.js
@@ -0,0 +1,36 @@
+export default {
+  estimateSection: {
+    title: "BS MOVE VOTRE Déménageur à Vaux-le-Pénil 77000",
+    content: {
+      main: "Déménagement à Vaux-le-Pénil ? BS Move s'occupe de tout, des cartons au transport. Service personnalisé et devis gratuit.",
+      secondary:
+        "Déménagement Vaux-le-Pénil sans stress avec BS Move !  Un expert évalue vos besoins sur place, on crée un devis personnalisé à prix attractif et on gère tout, de l'emballage à l'installation.  Profitez de votre nouveau chez-vous à Vaux-le-Pénil l'esprit léger.  Contactez BS Move dès aujourd'hui pour votre devis gratuit !",
+    },
+    actions: {
+      leftButton: "Obtenir un devis",
+      rightButton: "En savoir plus",
+    },
+  },
+  competencesSection: {
+    title: "Location de monte meuble et de camion avec chauffeur Vaux-le-Pénil",
+    subtitle: "BS Move Location a Vaux-le-Pénil, une équipe professionnelle.",
+    contents: {
+      firstPart:
+        "Choisissez la location d'un camion avec chauffeur chez BS Move à Vaux-le-Pénil, parfaitement adapté à vos besoins spécifiques. Nos experts offrent des solutions sur mesure et un service attentionné, prenant en charge le chargement et le transport de vos biens. Déménagez sereinement à Vaux-le-Pénil et profitez pleinement de votre nouvelle vie. Demandez dès aujourd'hui un devis gratuit pour un déménagement sans tracas !",
+      secondPart: "Déménagement à Vaux-le-Pénil compliqué par des objets encombrants ou des accès difficiles ? Pas de panique ! Louez un monte-meuble avec opérateur BS Move. Service sur mesure, matériel pro et équipe qualifiée pour un déménagement sans stress, même à Vaux-le-Pénil. Contactez-nous pour un devis gratuit !",
+    },
+    action: "demende la location en ligne",
+  },
+  meubleSection: {
+    title: "Achats de carton de déménagement Vaux-le-Pénil",
+    content: {
+      main: "Vous avez besoin de matériel d'emballage pour votre déménagement à Vaux-le-Pénil ? BS Move vous offre des cartons résistants et une vaste sélection de fournitures pour répondre à toutes vos exigences.",
+      secondary:
+        "Vaux-le-Pénil move made easy! BS Move offers top-notch packing supplies, a wide variety, and expert advice for a smooth, secure move. Get your free quote and enjoy our expertise.",
+    },
+    actions: {
+      leftButton: "Achats de cartons",
+      rightButton: "En savoir plus",
+    },
+  },
+}
--- helpers/routes.js
+++ helpers/routes.js
@@ -73,6 +73,8 @@ const Routes = {
   NEMOURS_PAGE: "/nemours",
   MORETLOINGETORVANNE_PAGE: "/moret-loing-et-orvanne",
   CLAYESOUILLY_PAGE: "/claye-souilly",
+  PROVINS_PAGE: "/provins",
+  VAUXLEPENIL_PAGE: "/vaux-le-penil",
 };
 
 export default Routes;
