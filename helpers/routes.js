79151f0e2b458f4c045862d129ed91f5f27e744d
hafed <hafed.zouyed@univ-constantine2.dz>
2024-07-16 11:57:26 +0100

    revert aadding provins and vaux-le-penil pages

--- helpers/routes.js
+++ helpers/routes.js
@@ -73,9 +73,6 @@ const Routes = {
   NEMOURS_PAGE: "/nemours",
   MORETLOINGETORVANNE_PAGE: "/moret-loing-et-orvanne",
   CLAYESOUILLY_PAGE: "/claye-souilly",
-  PROVINS_PAGE: "/provins",
-  VAUXLEPENIL_PAGE: "/vaux-le-penil",
-
 };
 
 export default Routes;
